5e5d074ef803daa1ecff7bf531555b26758a8a9b
Adewale Orotayo <didymus7007@gmail.com>
2020-09-01 23:29:17 +0100

    Added active hel helper to show current active link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def active_link(link_text, link_path)
+    class_name = current_page?(link_path) ? 'links hom' : 'links'
+
+    content_tag(:li, class: class_name) do
+      link_to link_text, link_path
+    end
+  end
 end
