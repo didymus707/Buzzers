4a474b533188d160dab88222a0dc85258a09631d
Adewale Orotayo <didymus7007@gmail.com>
2020-09-02 01:39:25 +0100

    Used cloudinary transformation on thumbnail version in image_uploader and removed logic from layout view(application.html.erb)

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -31,13 +31,14 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :standard do
-    eager
+    process :eager => true
     process resize_to_fill: [250, 250]
   end
 
   version :thumbnail do
-    eager
-    process resize_to_fit: [128, 128]
+    process :eager => true
+    # process resize_to_fit: [180, 180]
+    cloudinary_transformation height: 180, width: 180, crop: "scale"
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
